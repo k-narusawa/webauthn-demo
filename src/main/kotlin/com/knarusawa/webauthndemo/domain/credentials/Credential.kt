af553830c9d5183c7bcd34ea4e1db2b860f97a7a
k-narusawa <narusawakohei@team-lab.com>
2024-05-26 21:44:41 +0900

    refactor: 💡 不要なconverterを削除

--- src/main/kotlin/com/knarusawa/webauthndemo/domain/credentials/Credential.kt
+++ src/main/kotlin/com/knarusawa/webauthndemo/domain/credentials/Credential.kt
@@ -6,7 +6,6 @@ import com.knarusawa.webauthndemo.domain.credentials.converter.AuthenticatorExte
 import com.webauthn4j.authenticator.AuthenticatorImpl
 import com.webauthn4j.converter.AttestedCredentialDataConverter
 import com.webauthn4j.converter.AuthenticationExtensionsClientOutputsConverter
-import com.webauthn4j.converter.AuthenticatorDataConverter
 import com.webauthn4j.converter.AuthenticatorTransportConverter
 import com.webauthn4j.converter.util.ObjectConverter
 import com.webauthn4j.data.AuthenticatorTransport
@@ -19,88 +18,87 @@ import com.webauthn4j.data.extension.client.RegistrationExtensionClientOutput
 import com.webauthn4j.util.Base64UrlUtil
 
 class Credential private constructor(
-  val credentialId: String,
-  val userId: String,
-  val aaguid: AAGUID,
-  label: String,
-  val attestedCredentialData: AttestedCredentialData,
-  val attestationStatement: AttestationStatement,
-  val attestationStatementFormat: String,
-  val transports: Set<AuthenticatorTransport>,
-  val authenticatorExtensions: AuthenticationExtensionsAuthenticatorOutputs<RegistrationExtensionAuthenticatorOutput>?,
-  val clientExtensions: AuthenticationExtensionsClientOutputs<RegistrationExtensionClientOutput>?,
-  counter: Long,
+        val credentialId: String,
+        val userId: String,
+        val aaguid: AAGUID,
+        label: String,
+        val attestedCredentialData: AttestedCredentialData,
+        val attestationStatement: AttestationStatement,
+        val attestationStatementFormat: String,
+        val transports: Set<AuthenticatorTransport>,
+        val authenticatorExtensions: AuthenticationExtensionsAuthenticatorOutputs<RegistrationExtensionAuthenticatorOutput>?,
+        val clientExtensions: AuthenticationExtensionsClientOutputs<RegistrationExtensionClientOutput>?,
+        counter: Long,
 ) {
-  var label: String = label
-    private set
-  var counter: Long = counter
-    private set
+    var label: String = label
+        private set
+    var counter: Long = counter
+        private set
 
-  companion object {
-    private val objectConverter = ObjectConverter()
-    private val authenticatorDataConverter = AuthenticatorDataConverter(objectConverter)
-    private val attestedCredentialDataConverter = AttestedCredentialDataConverter(objectConverter)
-    private val transportConverter = AuthenticatorTransportConverter()
-    private val clientOutputsConverter =
-      AuthenticationExtensionsClientOutputsConverter(objectConverter)
+    companion object {
+        private val objectConverter = ObjectConverter()
+        private val attestedCredentialDataConverter = AttestedCredentialDataConverter(objectConverter)
+        private val transportConverter = AuthenticatorTransportConverter()
+        private val clientOutputsConverter =
+                AuthenticationExtensionsClientOutputsConverter(objectConverter)
 
-    fun of(
-      credentialId: ByteArray?,
-      userId: String,
-      authenticator: AuthenticatorImpl,
-    ): Credential {
-      val rawAAGUID = authenticator.attestedCredentialData.aaguid.value.toString()
-      val aaguid = AAGUID.fromAAGUID(rawAAGUID)
+        fun of(
+                credentialId: ByteArray?,
+                userId: String,
+                authenticator: AuthenticatorImpl,
+        ): Credential {
+            val rawAAGUID = authenticator.attestedCredentialData.aaguid.value.toString()
+            val aaguid = AAGUID.fromAAGUID(rawAAGUID)
 
-      return Credential(
-        credentialId = Base64UrlUtil.encodeToString(credentialId),
-        userId = userId,
-        aaguid = aaguid,
-        label = aaguid.labael,
-        attestedCredentialData = authenticator.attestedCredentialData,
-        attestationStatement = authenticator.attestationStatement
-          ?: throw IllegalArgumentException("AttestationStatement is not found"),
-        attestationStatementFormat = authenticator.attestationStatement!!.format,
-        transports = authenticator.transports ?: emptySet(),
-        authenticatorExtensions = authenticator.authenticatorExtensions,
-        clientExtensions = authenticator.clientExtensions,
-        counter = authenticator.counter,
-      )
+            return Credential(
+                    credentialId = Base64UrlUtil.encodeToString(credentialId),
+                    userId = userId,
+                    aaguid = aaguid,
+                    label = aaguid.labael,
+                    attestedCredentialData = authenticator.attestedCredentialData,
+                    attestationStatement = authenticator.attestationStatement
+                            ?: throw IllegalArgumentException("AttestationStatement is not found"),
+                    attestationStatementFormat = authenticator.attestationStatement!!.format,
+                    transports = authenticator.transports ?: emptySet(),
+                    authenticatorExtensions = authenticator.authenticatorExtensions,
+                    clientExtensions = authenticator.clientExtensions,
+                    counter = authenticator.counter,
+            )
+        }
+
+        fun from(record: CredentialsRecord) = Credential(
+                credentialId = record.credentialId,
+                userId = record.userId,
+                aaguid = AAGUID.fromAAGUID(record.aaguid),
+                label = record.label,
+                attestedCredentialData = attestedCredentialDataConverter.convert(
+                        Base64UrlUtil.decode(record.attestedCredentialData)
+                ),
+                attestationStatement = AttestationStatementConverter().convertToEntityAttribute(record.attestationStatement),
+                attestationStatementFormat = record.attestationStatementFormat,
+                transports = record.transports.split(",").map {
+                    transportConverter.convert(it)
+                }.toSet(),
+                authenticatorExtensions = AuthenticatorExtensionsConverter().convertToExtensions(
+                        Base64UrlUtil.decode(record.authenticatorExtensions)
+                ),
+                clientExtensions = record.clientExtensions?.let {
+                    clientOutputsConverter.convert(record.clientExtensions)
+                },
+                counter = record.counter,
+        )
     }
 
-    fun from(record: CredentialsRecord) = Credential(
-      credentialId = record.credentialId,
-      userId = record.userId,
-      aaguid = AAGUID.fromAAGUID(record.aaguid),
-      label = record.label,
-      attestedCredentialData = attestedCredentialDataConverter.convert(
-        Base64UrlUtil.decode(record.attestedCredentialData)
-      ),
-      attestationStatement = AttestationStatementConverter().convertToEntityAttribute(record.attestationStatement),
-      attestationStatementFormat = record.attestationStatementFormat,
-      transports = record.transports.split(",").map {
-        transportConverter.convert(it)
-      }.toSet(),
-      authenticatorExtensions = AuthenticatorExtensionsConverter().convertToExtensions(
-        Base64UrlUtil.decode(record.authenticatorExtensions)
-      ),
-      clientExtensions = record.clientExtensions?.let {
-        clientOutputsConverter.convert(record.clientExtensions)
-      },
-      counter = record.counter,
-    )
-  }
+    fun changeLabel(label: String) {
+        this.label = label
+    }
 
-  fun changeLabel(label: String) {
-    this.label = label
-  }
+    fun updateCounter(counter: Long) {
+        this.counter = counter
+    }
 
-  fun updateCounter(counter: Long) {
-    this.counter = counter
-  }
-
-  override fun toString(): String {
-    return """Credential(
+    override fun toString(): String {
+        return """Credential(
       credentialId='$credentialId', 
       userId='$userId', 
       aaguid='$aaguid',
@@ -114,5 +112,5 @@ class Credential private constructor(
       counter=$counter
     )
     """.trimIndent()
-  }
+    }
 }
